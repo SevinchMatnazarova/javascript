c1b7dcaac324f38d6af7841450654f204acc9fd9
SevinchMatnazarova <sevinchmatnazarova940@gmail.com>
2025-12-04 15:18:24 +0500

    javascript

--- app.js
+++ app.js
@@ -4,3 +4,15 @@ let str = "atfyd";
 const booln = false;
 
 console.log(typeof booln)
+
+
+let x = 14;
+let y = 15;
+
+if (x == y) {
+    console.log("Ikkalasi teng ")
+} else if (x >  y) {
+    console.log(" x:", x)
+} else {
+    console.log("y:", y)
+}
