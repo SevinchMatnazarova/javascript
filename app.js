3629b0be53cf6a8b54de791a2b4f98780804e43b
SevinchMatnazarova <sevinchmatnazarova940@gmail.com>
2025-12-04 16:25:41 +0500

    javascript

--- app.js
+++ app.js
@@ -20,17 +20,50 @@
 // x > y ? console.log("x:" , x): console.log("y:", y);
 // x ==  y? console.log("ikkalasi teng ") : x > y ? console.log("x:", x)  : console.log("y:", y) 
 
-let day1 = "dushnba";
-let day2 = "shanba";
+// let day1 = "dushnba";
+// let day2 = "shanba";
 
-switch ("shanba"){
-    case "Dushanba":
-        console.log(` Bugun ${day1}`);
-        break;
+// switch ("shanba"){
+//     case "Dushanba":
+//         console.log(` Bugun ${day1}`);
+//         break;
 
-    case "Shanba":
-        console.log(`Bugun ${day2}`);
-        break;
-    default:
-         console.log(":(");
+//     case "Shanba":
+//         console.log(`Bugun ${day2}`);
+//         break;
+//     default:
+//          console.log(":(");
+// }
+
+
+// let a = 2;
+// let b = -1;
+// let c = -3;
+
+// if (a >=b && b >=c){
+//     a=a*2
+//     b=b*2
+//     c=c*2
+//     console.log( a)
+//     console.log( b)
+//     console.log( c)
+// }else{
+//     a = Math.abc(a);
+//     b = Math.abc(b);
+//     c = Math.abc(c);
+    
+// }
+
+let x = 0.54;
+let y = 0.09;
+let z = 1.64;
+
+if (x >= 1 && x <= 3) {
+    console.log(x);
+}
+if (y >= 1 && y <= 3) {
+    console.log(y);
+}
+if (z >= 1 && z <= 3) {
+    console.log(z);
 }
