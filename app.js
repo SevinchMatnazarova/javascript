81bc69dc78588471e4f96751a2b9679f8848a1a1
SevinchMatnazarova <sevinchmatnazarova940@gmail.com>
2025-12-04 16:03:57 +0500

    javascript

--- app.js
+++ app.js
@@ -1,18 +1,36 @@
-console.log("salom");
-let num = 2;
-let str = "atfyd";
-const booln = false;
+// console.log("salom");
+// let num = 2;
+// let str = "atfyd";
+// const booln = false;
 
-console.log(typeof booln)
+// console.log(typeof booln)
 
 
-let x = 14;
-let y = 15;
+// let x = 14;
+// let y = 15;
 
-if (x == y) {
-    console.log("Ikkalasi teng ")
-} else if (x >  y) {
-    console.log(" x:", x)
-} else {
-    console.log("y:", y)
+// if (x == y) {
+//     console.log("Ikkalasi teng ")
+// } else if (x >  y) {
+//     console.log(" x:", x)
+// } else {
+//     console.log("y:", y)
+// }
+
+// x > y ? console.log("x:" , x): console.log("y:", y);
+// x ==  y? console.log("ikkalasi teng ") : x > y ? console.log("x:", x)  : console.log("y:", y) 
+
+let day1 = "dushnba";
+let day2 = "shanba";
+
+switch ("shanba"){
+    case "Dushanba":
+        console.log(` Bugun ${day1}`);
+        break;
+
+    case "Shanba":
+        console.log(`Bugun ${day2}`);
+        break;
+    default:
+         console.log(":(");
 }
