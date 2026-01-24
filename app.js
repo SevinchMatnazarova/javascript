3c0d178fa9872d98b6ba67116781db6d8ea1e863
SevinchMatnazarova <sevinchmatnazarova940@gmail.com>
2026-01-24 16:14:32 +0500

    javascript

--- app.js
+++ app.js
@@ -54,16 +54,26 @@
     
 // }
 
-let x = 0.54;
-let y = 0.09;
-let z = 1.64;
+// let x = 0.54;
+// let y = 0.09;
+// let z = 1.64;
 
-if (x >= 1 && x <= 3) {
-    console.log(x);
+// if (x >= 1 && x <= 3) {
+//     console.log(x);
+// }
+// if (y >= 1 && y <= 3) {
+//     console.log(y);
+// }
+// if (z >= 1 && z <= 3) {
+//     console.log(z);
+// }
+
+// skill 16
+let a = 3;
+let  b= 3;
+let c = 4;
+let y = 0;
+for (let h = a; h <= c;  h += 3 ){
+    y = Math.cbrt((a * h + b) / (b ** 2 + Math.cos(h) ** 2)) - Math.sin(h ** 2) / (a * b);
 }
-if (y >= 1 && y <= 3) {
-    console.log(y);
-}
-if (z >= 1 && z <= 3) {
-    console.log(z);
-}
+console.log(y)
